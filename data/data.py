2382488bc18b3a6b6855c0362eb5d9a4523f2fe5
PiSaucer <33438654+PiSaucer@users.noreply.github.com>
2024-04-23 15:02:44 -0400

    Update data.py

--- data/data.py
+++ data/data.py
@@ -5,13 +5,17 @@ def collect_courses(directory):
     courses = {}
     for root, dirs, files in os.walk(directory):
         for filename in files:
-            if filename.endswith(".json") and filename != "data.json":  # Exclude "data.json"
+            if filename.endswith(".json") and filename != "data.json" and filename != "data.old.json":  # Exclude "data.json"
                 print(f"Found {os.path.join(root, filename)}")
                 course_name = os.path.basename(root)
                 if course_name not in courses:
                     courses[course_name] = []
-                courses[course_name].append(filename[:-5])  # Exclude the last 5 characters (.json)
-    return courses
+                courses[course_name].append(filename)
+
+    # Sort the courses dictionary based on course name and file name
+    sorted_courses = {course: sorted(files, key=lambda x: (x.split('-')[0], x.split('-')[1]), reverse=True) for course, files in courses.items()}
+    sorted_courses = dict(sorted(sorted_courses.items(), key=lambda x: x[0], reverse=True))  # Sort by course name in reverse order
+    return sorted_courses
 
 def main():
     directory = "."  # Assuming the script is in the directory containing the course folders
